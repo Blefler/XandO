11d37a675abec8d0c1a4d60d514a4e93e51841f4
Blefler <blefler@mail.csuchico.edu>
2018-03-27 08:00:35 -0700

    Fixed grammar

--- main.cpp
+++ main.cpp
@@ -53,9 +53,9 @@ int main()
         }
     }
         cout<<"\nBOARD\n-----\n";
-        cout<<board[0][0]<<" "<<board[0][1]<<" "<<board[0][2]<<endl;
-        cout<<board[1][0]<<" "<<board[1][1]<<" "<<board[1][2]<<endl;
-        cout<<board[2][0]<<" "<<board[2][1]<<" "<<board[2][2]<<endl;
+        cout<<board[0][0]<<" "<<board[0][1]<<" "<<board[0][2]<<" "<<endl;
+        cout<<board[1][0]<<" "<<board[1][1]<<" "<<board[1][2]<<" "<<endl;
+        cout<<board[2][0]<<" "<<board[2][1]<<" "<<board[2][2]<<" "<<endl;
     
   }while( playing );
 
