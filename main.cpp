050bd792b73c9673ad5c5557e95aef24279e1e08
Blefler <blefler@mail.csuchico.edu>
2018-04-05 15:52:33 -0700

    Fixed for real this time guys.

--- main.cpp
+++ main.cpp
@@ -65,24 +65,27 @@ int main()
 
 for (int i = 0; i<2; i++)
   {
-   if ((board[i][0]!=' ')&&(board[i][0]==board[i][1])&&(board[i][0]==board[i][2]))
+   if ((board[i][0]!=BLANK)&&(board[i][1]!=BLANK)&&(board[i][2]!=BLANK)&&(board[i][0]==board[i][1])&&(board[i][0]==board[i][2]))
      {
       playing = false;
       won = true;
-      winner = board[i][0]; 
+      winner = board[i][0];
+      cout<<"1"<<endl;
      }
-   if ((board[0][i]!=' ')&&(board[0][i]==board[1][i])&&(board[0][i]==board[2][i]))
+   if ((board[0][i]!=BLANK)&&(board[1][i]!=BLANK)&&(board[2][i]!=BLANK)&&(board[0][i]==board[1][i])&&(board[0][i]==board[2][i]))
      {
       playing = false;
       won = true;
       winner = board[0][i];
+      cout<<"2"<<endl;
      }  
   }
-if ((board[1][1]!=' ')&&((board[1][1]==board[0][0])&&(board[1][1]==board[2][2]))||((board[1][1]==board[0][2])&&(board[1][1]==board[2][0])))
+if (board[1][1]!=BLANK&&((board[1][1]==board[0][0]&&board[1][1]==board[2][2])||(board[1][1]==board[0][2]&&board[1][1]==board[2][0])))
   {
    playing = false;
    won = true;
    winner = board[1][1];
+   cout<<"3"<<endl;
   }
   
   }while( playing );
