23c9222586ada677d74bd80991332ef5cc68be12
Blefler <blefler@mail.csuchico.edu>
2018-04-05 14:28:14 -0700

    Fixed better

--- main.cpp
+++ main.cpp
@@ -65,20 +65,20 @@ int main()
 
 for (int i = 0; i<2; i++)
   {
-   if ((board[i][0]==board[i][1])&&(board[i][0]==board[i][2]))
+   if ((board[i][0]!=' ')&&(board[i][0]==board[i][1])&&(board[i][0]==board[i][2]))
      {
       playing = false;
       won = true;
       winner = board[i][0]; 
      }
-   if ((board[0][i]==board[1][i])&&(board[0][i]==board[2][i]))
+   if ((board[0][i]!=' ')&&(board[0][i]==board[1][i])&&(board[0][i]==board[2][i]))
      {
       playing = false;
       won = true;
       winner = board[0][i];
      }  
   }
-if (((board[1][1]==board[0][0])&&(board[1][1]==board[2][2]))||((board[1][1]==board[0][2])&&(board[1][1]==board[2][0])))
+if ((board[1][1]!=' ')&&((board[1][1]==board[0][0])&&(board[1][1]==board[2][2]))||((board[1][1]==board[0][2])&&(board[1][1]==board[2][0])))
   {
    playing = false;
    won = true;
