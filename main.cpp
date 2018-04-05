e13b612627ec5a45a8c2592f3219694cc5943480
Blefler <blefler@mail.csuchico.edu>
2018-04-05 15:54:41 -0700

    Fixed for real this time guys.

--- main.cpp
+++ main.cpp
@@ -70,14 +70,14 @@ for (int i = 0; i<2; i++)
       playing = false;
       won = true;
       winner = board[i][0];
-      cout<<"1"<<endl;
+//      cout<<"Win condition 1"<<endl;
      }
    if ((board[0][i]!=BLANK)&&(board[1][i]!=BLANK)&&(board[2][i]!=BLANK)&&(board[0][i]==board[1][i])&&(board[0][i]==board[2][i]))
      {
       playing = false;
       won = true;
       winner = board[0][i];
-      cout<<"2"<<endl;
+//      cout<<"Win Condition 2"<<endl;
      }  
   }
 if (board[1][1]!=BLANK&&((board[1][1]==board[0][0]&&board[1][1]==board[2][2])||(board[1][1]==board[0][2]&&board[1][1]==board[2][0])))
@@ -85,7 +85,7 @@ if (board[1][1]!=BLANK&&((board[1][1]==board[0][0]&&board[1][1]==board[2][2])||(
    playing = false;
    won = true;
    winner = board[1][1];
-   cout<<"3"<<endl;
+//   cout<<"Win Condition 3"<<endl;
   }
   
   }while( playing );
